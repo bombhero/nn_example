53dc4e3ff19633a096e91a4ed292c93d2b7ef4a9
MarkZhang <bomb.zhang@gamil.com>
2018-05-28 21:39:14 +0800

    Add a neat example

--- neat_example/neat_example.py
+++ neat_example/neat_example.py
@@ -1,16 +1,48 @@
 import os
 import neat
+import numpy as np
 from visualize import draw_net
 from tensorflow.examples.tutorials.mnist.input_data import read_data_sets
+
+mnist = read_data_sets("../data")
+
+
+def trans_to_one_shot(x):
+    rows_num = len(x)
+    one_hot = np.zeros([rows_num, 10])
+    for i in range(rows_num):
+        one_hot[i, x[i]] = 1
+    return one_hot
+
+
+def calc_fitness(genomes, config):
+    global mnist
+    for g_id, genome in genomes:
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        genome.fitness = 1280
+        batch_x, batch_y = mnist.train.next_batch(128)
+        one_y = trans_to_one_shot(batch_y)
+        for in_x, out_y in zip(batch_x, one_y):
+            result_y = np.array(net.activate(in_x))
+            genome.fitness -= np.sum((result_y - out_y) ** 2)
+
 
 def main():
     config_path = os.path.dirname(__file__)
     config_file = os.path.join(config_path, "neat.conf")
-    mnist = read_data_sets("../data")
-    pass
+    neat_config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                              neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                              config_file)
+    p = neat.Population(neat_config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(5))
+    winner = p.run(calc_fitness, 10)
+    draw_net(neat_config, winner, True, fmt='png')
 
 
-if if __name__ == '__main__':
+if __name__ == '__main__':
     main()
 
 
