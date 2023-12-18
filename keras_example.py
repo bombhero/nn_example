9eb035be4fd1da6f8f9d76554a5f856316a49f88
Mark <bombhero@users.noreply.github.com>
2023-12-18 11:45:13 +0800

    Adapt for tensorflow 2.

--- keras_example.py
+++ keras_example.py
@@ -1,36 +1,35 @@
 import numpy as np
 import time
-import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
 from mnist_read import read_mnist
-from tensorflow.keras import layers
 
 
 def build_model(verbose=0):
-    input_data = tf.keras.Input(shape=(784,), name="Input")
-    x = layers.BatchNormalization()(input_data)
-    y = layers.Dense(10, activation="softmax")(x)
+    input_data = keras.Input(shape=(784,), name="Input")
+    x = keras.layers.BatchNormalization()(input_data)
+    y = keras.layers.Dense(10, activation="softmax")(x)
     model = tf.keras.Model(inputs=input_data, outputs=y, name="MnistModel")
 
     model.summary()
 
-    model.compile(loss='categorical_crossentropy',
-                  optimizer=tf.keras.optimizers.Adam(lr=0.01, decay=0.01),
+    model.compile(loss=keras.metrics.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adam(lr=0.01),
                   metrics=['accuracy'])
     return model
 
 
-def draw_number(ax, X):
-    matrix = np.reshape(X, [28, 28])
+def draw_number(ax, x):
+    matrix = np.reshape(x, [28, 28])
     ax.imshow(matrix)
 
 
 def trans_to_one_shot(X):
     rows_num = len(X)
-    oneX = np.zeros([rows_num, 10])
+    one_x = np.zeros([rows_num, 10])
     for i in range(rows_num):
-        oneX[i, X[i]] = 1
-    return oneX
+        one_x[i, X[i]] = 1
+    return one_x
 
 
 def main():
@@ -43,18 +42,17 @@ def main():
         batch_x, batch_y = mnist.train_data, mnist.train_label
         one_y = trans_to_one_shot(batch_y)
 
-        batch_size = batch_x.shape[0]
+        batch_size = 256
         nb_epoch = 5
 
-        # early_stopping = EarlyStopping(monitor='loss', patience=2)
-        hist = nn_model.fit(batch_x, one_y, batch_size=batch_size, epochs=nb_epoch)
+        hist = nn_model.fit(batch_x, one_y, batch_size=batch_size, epochs=nb_epoch, verbose=1)
         new_ts = time.time()
         spend_time = new_ts - ts
 
         test_x, test_y = mnist.test_data, mnist.test_label
-        predict_y = np.argmax(nn_model.predict(test_x), axis=1)
-        correct = (test_y.reshape([test_y.shape[0]]) == predict_y).sum()
-        print("{}/{}".format(correct, test_y.shape[0]))
+        test_y = trans_to_one_shot(test_y)
+        loss, acc = nn_model.evaluate(test_x, test_y, batch_size=256, verbose=1)
+        print('Test loss {:>.3f}, Accuracy {:.3f}'.format(loss, acc))
 
 
 if __name__ == "__main__":
